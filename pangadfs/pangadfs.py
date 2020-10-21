4e0bcb90d80def7f1feee2481a659442fa09ed72
sansbacon <eric@erictruett.com>
2020-10-20 20:26:09 -0500

    Basic implementation works - add mutation next

--- pangadfs/pangadfs.py
+++ pangadfs/pangadfs.py
@@ -7,7 +7,11 @@ currently based on DK, maybe more flexible in future
 """
 
 # %%
+import itertools
 import logging
+from multiprocessing import Pool, cpu_count
+import random
+import uuid
 
 import pandas as pd
 import numpy as np
@@ -17,8 +21,17 @@ import numpy as np
 class PanGaDFS:
 
     DEFAULT_FLEX = ('RB', 'WR', 'TE')
+    SALARY_CAP = 50000
+    SALARY_FLOOR = 45000
+    MIN_SCORE = 120
 
-    def __init__(self, player_pool, flex=DEFAULT_FLEX):
+    def __init__(self, 
+                 player_pool, 
+                 flex=DEFAULT_FLEX, 
+                 initial_size=500, 
+                 mutation_rate=.05,
+                 projcol='proj',
+                 roulette_method='proj'):
         """Creates instance
 
         Args:
@@ -38,11 +51,17 @@ class PanGaDFS:
         self._weighted_tes = None
         self._weighted_dsts = None
         self._weighted_flex = None
-
+        self.lineups = None
+        self.initial_size = initial_size
+        self.mutation_rate = mutation_rate
+        self.fitness_func = None
+        self.projcol = projcol
+        self.roulette_method = roulette_method
+        
     @property
     def player_pool(self):
         if self._weighted_player_pool is None:
-            self._weighted_player_pool = self.roulette_wheel()
+            self._weighted_player_pool = self.roulette_wheel(self.roulette_method)
         return self._weighted_player_pool
 
     @property
@@ -81,40 +100,124 @@ class PanGaDFS:
             self._weighted_wrs = self._weighted_pos('WR')
         return self._weighted_wrs
 
+    def _breed_two(self, lineups, lineup_size=9):
+        """Breeds two lineups"""
+        while True:
+            # randomly assign father/mother for each lineup slot
+            # this should be a 9-item array of zeros and ones            
+            mother_or_father = np.random.binomial(1, .5, size=lineup_size)
+
+            # picks mother or father for each position
+            # have to use index slice to get dataframe instead of series
+            new_lineup = pd.concat([
+                lineups[parent].iloc[idx:idx+1, :]
+                for idx, parent in enumerate(mother_or_father)
+            ])
+
+            # end loop if valid lineup, assign new uuid
+            # no need to assign id or calc fitness if lineup invalid
+            if self.is_valid_lineup(new_lineup):
+                new_lineup['lid'] = uuid.uuid4()
+                new_lineup['fitness'] = self.fitness(new_lineup)
+                return new_lineup
+
     def _weighted_pos(self, pos):
         """Gets weighted position"""
         return self.player_pool.query(f'pos == "{pos}"')
 
-    def create_lineup(self):
-        """Creates lineup from df given rules"""
-        # get position minimums + extra FLEXES
-        return (
-            pd.concat([
-              self.qbs.sample(1, weights=self.qbs['weight']),
-              self.rbs.sample(2, weights=self.rbs['weight']),
-              self.wrs.sample(3, weights=self.wrs['weight']),
-              self.tes.sample(1, weights=self.tes['weight']),
-              self.dsts.sample(1, weights=self.dsts['weight']),
-              self.flex.sample(8, weights=self.flex['weight'])
-            ])
-            .drop_duplicates(subset='player_id')
-            .iloc[0:9, :]
+    def breed(self):
+        """Breeds lineups together
+           Default is to drop 1/2 of worst lineups and breed remainder
+        """
+        new_lineups = []
+
+        # give weight in breeding to higher-fitness lineups
+        lineup_fitness = (
+            self.lineups
+            .groupby('lid')
+            .first()
+            .loc[:, ['fitness']]
         )
 
-    def roulette_wheel(self, projcol='proj'):
-        """Creates weighted player pool by projection"""
-        df = self._player_pool.copy()
-        df['weight'] = 0
-        for pos in df['pos'].unique():
-            df.loc[df['pos'] == pos, 'weight'] = (
-                df.loc[df['pos'] == pos, projcol] / df.loc[df['pos'] == pos, projcol]
-                .sum()
+        weights = lineup_fitness['fitness'] / lineup_fitness['fitness'].sum()
+
+        # make new lineups
+        for _ in range(len(lineup_fitness)):
+            lid1, lid2 = lineup_fitness.sample(2, weights=weights).index
+            lu1 = self.lineups.query(f'lid == {lid1}')
+            lu2 = self.lineups.query(f'lid == {lid2}')
+            new_lineups.append(self._breed_two((lu1, lu2)))
+
+        return pd.concat(new_lineups)
+
+    def create_lineup(self, lineup_id=None):
+        """Creates lineup from df given rules"""
+        i = 1
+        while True:
+            # supply unique lineup id if none specified
+            if not lineup_id:
+                lineup_id = uuid.uuid4()
+
+            # get position minimums + extra FLEXES
+            lineup = (
+              pd.concat([
+                self.qbs.sample(1, weights=self.qbs['weight']),
+                self.rbs.sample(2, weights=self.rbs['weight']),
+                self.wrs.sample(3, weights=self.wrs['weight']),
+                self.tes.sample(1, weights=self.tes['weight']),
+                self.dsts.sample(1, weights=self.dsts['weight']),
+                self.flex.sample(8, weights=self.flex['weight'])
+              ])
+              .drop_duplicates(subset='player_id')
+              .iloc[0:9, :]
+              .assign(lid=lineup_id)
             )
+
+            # return if valid lineup
+            if self.is_valid_lineup(lineup):
+                logging.info(f'Took {i} times to find valid')
+                return lineup
+            i += 1
+
+    def fitness(self, lineups):
+        """Assesses fitness of individual"""
+        if not self.fitness_func:
+            return lineups.groupby('lid')[self.projcol].transform('sum')       
+        else:
+            return self.fitness_func(lineups)
+
+    def initial_population(self):
+        """Creates initial population of lineups"""
+        with Pool(processes=cpu_count()) as p:
+            lineups = p.map(self.create_lineup, range(1, self.initial_size + 1))
+        self.lineups = pd.concat(lineups)
+        self.lineups['fitness'] = self.fitness(self.lineups)
+        return self.lineups
+
+    def is_valid_lineup(self, lineup):
+        """Tests if valid lineup"""
+        sal = lineup.salary.sum()
+        proj = lineup.proj.sum()
+        return ((self.SALARY_CAP >= sal and
+                 sal >= self.SALARY_FLOOR) and
+                 proj >= self.MIN_SCORE and
+                 len(lineup.player_id.unique()) == 9)
+
+    def roulette_wheel(self, projcol='proj'):
+        """Creates weighted player pool by projection
+           Can do on entire player pool as pandas will normalize weights
+        """
+        df = self._player_pool.copy()
+        if projcol == 'composite':
+            # create composite weight from floor/mean/ceil
+            # convert to value and scale into weight
+            composite = np.sqrt((df['floor']**2 + df['ceil']**2 + df['proj'] **2) / 3)
+            value = composite / df['salary'] * 1000
+            df['weight'] = value / value.sum()
+        else:
+            df['weight'] = df[projcol] / df[projcol].sum()
         return df
 
-    def selection(self, df, n):
-        """Initial selection of population"""
-        pass
 
 # %%
 if __name__ == '__main__':
--- tests/test_pangadfs.py
+++ tests/test_pangadfs.py
@@ -1,9 +1,25 @@
 # -*- coding: utf-8 -*-
 # tests/test_db.py
+import logging
+import random
 import pandas as pd
 import pytest
 
 from pangadfs import *
+
+
+LOGGER = logging.getLogger(__name__)
+PLAYERS_IN_LINEUP = 9
+
+
+@pytest.fixture
+def initial_size():
+    return random.randint(25, 100)
+
+
+@pytest.fixture
+def lu(test_directory):
+    return pd.read_csv(test_directory / 'lineups.csv')
 
 
 @pytest.fixture
@@ -14,4 +30,30 @@ def players(test_directory):
 def test_players(players):
     """Tests team_counts"""
     assert isinstance(players, pd.core.api.DataFrame)
+
+
+def test_initial_population(benchmark, players, initial_size, tprint):
+    pg = PanGaDFS(player_pool=players, 
+                  initial_size=initial_size,
+                  roulette_method='composite')
+    lineups = benchmark.pedantic(pg.initial_population, iterations=1, rounds=2)
+    lineup = lineups.iloc[0:9, :]
+    tprint(lineup)
+    tprint((round(lineup.proj.sum(), 2), lineup.salary.sum()))
+    assert len(lineups) == initial_size * PLAYERS_IN_LINEUP
+
+
+def test_breed_two(lu, tprint):
+    pg = PanGaDFS(player_pool=players)
+    two_lu = (lu.iloc[0:9, :], lu.iloc[9:18, :])
+    new_lu = pg._breed_two(two_lu)
+    assert len(new_lu) == len(two_lu[0])
+
+
+def test_breed(players, tprint):
+    pg = PanGaDFS(player_pool=players, 
+                  initial_size=10)
+    lineups = pg.initial_population()
+    new_lineups = pg.breed()
+    assert len(new_lineups) == len(lineups)
 
