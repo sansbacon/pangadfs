bf1b5312b879cdec294a4a1ca9d5b18f64b7f739
Eric Truett <sansbacon@gmail.com>
2025-07-23 18:37:51 -0500

    Remove gui app from main library (#21)
    * change to README
    
    * removed the gui app from the main library
    
    ---------
    
    Co-authored-by: Eric Truett <eruett@alas.com>

--- pangadfs/gui/widgets/__init__.py
+++
@@ -1,8 +0,0 @@
-# pangadfs/gui/widgets/__init__.py
-# -*- coding: utf-8 -*-
-# Copyright (C) 2020 Eric Truett
-# Licensed under the MIT License
-
-from .player_pool_panel import PlayerPoolPanel
-
-__all__ = ['PlayerPoolPanel']
